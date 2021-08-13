1dcb77ef55295424d37c5f6012388f74b59ad8c0
ncyxie <mantukasjolanta@gmail.com>
2021-08-14 00:58:21 +0300

    Disabled 'Windows (new)' GUI style for Linux OS (it doesn't work)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -59,6 +59,12 @@ MainWindow::MainWindow(QWidget *parent)
     ui->action_gui_style_Windows_new->setCheckable(true);
     ui->action_gui_style_Fusion->setChecked(true);
 
+    #ifdef _WIN32
+    ui->action_gui_style_Windows_new->setEnabled(true);
+    #elif linux
+    ui->action_gui_style_Windows_new->setDisabled(true);
+    #endif
+
     ui->action_statusBar_On->setChecked(false);
     ui->action_statusBar_Off->setChecked(true);
     ui->action_Word_Counter_On->setDisabled(true);

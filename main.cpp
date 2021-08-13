4ee8806018a501cede81344779134b11361906c1
ncyxie <mantukasjolanta@gmail.com>
2021-08-13 20:55:32 +0300

    Added application icon to titlebar for About and Discord dialogs

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ int main(int argc, char *argv[])
     app.setOrganizationName("ncyxie");
     app.setApplicationName("Notepad DOT Qt");
     app.setApplicationVersion("1.0.1-s2");
-    app.setWindowIcon(QIcon(":/img/Notepad-DOT-icon.ico"));
     MainWindow w;
     w.show();
 
